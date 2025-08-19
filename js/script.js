a73e9ec0d735f4effdaaaa5f730f0ea7131f197c
Antoine Petit <antooine.petit@gmail.com>
2025-08-19 14:24:37 +0200

    feat: enhance product display and cart
    - Improved product card layout
    - Added add to cart functionality
    - Implemented quantity adjustment
    - Updated styling for better UX
    - Refined cart display

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 async function getDatas() {
   const req = await fetch("../data.json");
   const res = await req.json();
-  console.log(res);
   return res;
 }
 
 async function populateDatas() {
   const datas = await getDatas();
   const productList = document.querySelector("#product-list");
+  let i = 1;
   datas.forEach((produit) => {
     const { category, name, price } = produit;
     let image;
-    console.log(window.innerWidth);
     if (window.innerWidth > 1024) {
       image = produit.image.desktop;
     }
@@ -21,18 +20,30 @@ async function populateDatas() {
     if (window.innerWidth <= 767) {
       image = produit.image.mobile;
     }
+    //             <div class="add"><button class="add-button"><img src="assets/images/icon-add-to-cart.svg" alt="Icon add to cart">Add to cart</button></div>
+
+    // <div class="in-cart">
+    //   <button class="decrement"><img src="assets/images/icon-decrement-quantity.svg" alt="Icon to decrement quantity"></button>
+    //   1
+    //   <button class="increment"><img src="assets/images/icon-increment-quantity.svg" alt="Icon to decrement quantity"></button>
+    // </div>
+
     const template = `
         <article>
           <div>
             <img src="${image}" alt="${name}">
-            <a href="#" class="add-button"><img src="assets/images/icon-add-to-cart.svg" alt="Icon add to cart">Add to cart</a>
+            <div class="in-cart">
+              <button class="decrement"><img src="assets/images/icon-decrement-quantity.svg" alt="Icon to decrement quantity"></button>
+              1
+              <button class="increment"><img src="assets/images/icon-increment-quantity.svg" alt="Icon to decrement quantity"></button>
+            </div>
           </div>
           <p class="subtitle">${category}</p>
           <h3>${name}</h3>
-          <p class="price-tag">$${price}</p>
+          <p class="price-tag">$${price.toFixed(2)}</p>
         </article>
-    `
-    productList.insertAdjacentHTML('beforeend', template)
+    `;
+    productList.insertAdjacentHTML("beforeend", template);
   });
 }
 
